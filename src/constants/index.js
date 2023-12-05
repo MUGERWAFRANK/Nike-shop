05e30a4f8edc1bf1992a1d19b07a5b4ed452ee64
mugerwa frank <fsimple47@gmail.com>
2023-12-05 05:16:47 +0300

    change

--- src/constants/index.js
+++ src/constants/index.js
@@ -112,7 +112,7 @@ export const footerLinks = [
         title: "Get in touch",
         links: [
             { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "+256778880079", link: "tel:+256705718118" },
         ],
     },
 ];
